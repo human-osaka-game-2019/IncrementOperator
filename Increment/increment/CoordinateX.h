86d0627501dffb1e3fa01c12be8d97dfcc3c1bfa
mono1140 <11potato30@gmail.com>
2019-11-19 12:03:03 +0900

    CoordinateXクラスの追加

---
+++ Increment/increment/CoordinateX.h
@@ -0,0 +1,27 @@
+﻿#ifndef COORDINATEX_H
+#define COORDINATEX_H
+
+class CoordinateX
+{
+private:
+
+	int value;
+
+public:
+
+	CoordinateX(int x) : value(x) {}
+	int Value() { return value; }
+
+	CoordinateX& operator ++()
+	{
+		return *this;
+	}
+
+	CoordinateX operator++(int)
+	{
+		return *this;
+	}
+
+};
+
+#endif
