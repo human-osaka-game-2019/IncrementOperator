c9b4c98378c2882e03687c0380aac05b108aed9c
taishi-matsumura <taishi0706@gmail.com>
2019-11-19 11:29:36 +0900

    :white_check_mark: テストコードを追加

---
+++ matsumura/IncrementOperator/IncrementOperatorTest/CoordinateX_Test.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+
+#include "../IncrementOperator/CoordinateX.h"
+
+TEST(CoordinateXTest, PostfixIncrementTest) {
+	///////////////////////////////////////////
+	// Arrange
+	CoordinateX x(5);
+
+	///////////////////////////////////////////
+	// Act
+	CoordinateX result = x++;
+
+	///////////////////////////////////////////
+	// Assert
+	// 元のインスタンスのvalueがincrementされていること
+	EXPECT_EQ(x.Value(), 6);
+
+	// 戻り値で返されるインスタンスはincrement前の状態であること
+	EXPECT_EQ(result.Value(), 5);
+}
+
+TEST(CoordinateXTest, PrefixIncrementTest) {
+	///////////////////////////////////////////
+	// Arrange
+	CoordinateX x(5);
+
+	///////////////////////////////////////////
+	// Act
+	CoordinateX result = ++x;
+
+	///////////////////////////////////////////
+	// Assert
+	// 元のインスタンスのvalueがincrementされていること
+	EXPECT_EQ(x.Value(), 6);
+
+	// 戻り値で返されるインスタンスもincrement後の状態であること
+	EXPECT_EQ(result.Value(), 6);
+}
