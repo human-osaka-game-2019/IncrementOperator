0a167c0c9202837799ff62f2d758374891dd2ad7
mono1140 <11potato30@gmail.com>
2019-11-19 12:07:26 +0900

    テストコードの修正

--- Increment/Sample-Test1/test.cpp
+++ Increment/Sample-Test1/test.cpp
@@ -1,15 +1,36 @@
 #include "pch.h"
 #include"../Increment/CoordinateX.h"
-int a = 0;
+
 TEST(CoordinateX_Test, Text_1)
 {
-	
+	int a = 0;
 	EXPECT_EQ(0, a++);
 }
 
 TEST(CoordinateX_Test, Text_2)
 {
-
+	int a = 0;
 	EXPECT_EQ(1, ++a);
+
+}
+
+TEST(CoordinateX_Test, Text_3)
+{
+	int a = 3;
+	EXPECT_EQ(4, ++a);
+
+}
+
+TEST(CoordinateX_Test, Text_4)
+{
+	int a = 3;
+	EXPECT_EQ(3, a++);
+
+}
+
+TEST(CoordinateX_Test, Text_5)
+{
+	int a = 6;
+	EXPECT_EQ(7, ++a);
 
 }
