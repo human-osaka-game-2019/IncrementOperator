3df599fad2e382ca708ae4810345a659bd2f3f14
mono1140 <11potato30@gmail.com>
2019-11-19 12:26:27 +0900

    ゲッターを使用したテストコードの追加

--- Increment/Sample-Test1/test.cpp
+++ Increment/Sample-Test1/test.cpp
@@ -1,10 +1,12 @@
 #include "pch.h"
 #include"../Increment/CoordinateX.h"
 
+int a = 0;
+
 TEST(CoordinateX_Test, Text_1)
 {
 	int a = 0;
-	EXPECT_EQ(0, a++);
+	EXPECT_EQ(0,a++);
 }
 
 TEST(CoordinateX_Test, Text_2)
@@ -32,5 +34,57 @@ TEST(CoordinateX_Test, Text_5)
 {
 	int a = 6;
 	EXPECT_EQ(7, ++a);
+
+}
+
+TEST(CoordinateX_Test, Text_6)
+{
+
+	CoordinateX x(0);
+
+	CoordinateX xx(0);
+
+	xx = x++;
+
+	EXPECT_EQ(0,xx.Value());
+
+}
+
+TEST(CoordinateX_Test, Text_7)
+{
+
+	CoordinateX x(4);
+
+	CoordinateX xx(0);
+
+	xx = x++;
+
+	EXPECT_EQ(4, xx.Value());
+
+}
+
+TEST(CoordinateX_Test, Text_8)
+{
+
+	CoordinateX x(5);
+
+	CoordinateX xx(0);
+
+	xx = ++x;
+
+	EXPECT_EQ(6, xx.Value());
+
+}
+
+TEST(CoordinateX_Test, Text_9)
+{
+
+	CoordinateX x(3);
+
+	CoordinateX xx(0);
+
+	xx = ++x;
+
+	EXPECT_EQ(4, xx.Value());
 
 }
