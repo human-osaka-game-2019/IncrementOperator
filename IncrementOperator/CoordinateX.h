414382d2aa38517d2cecbbd0c754551d8605ba8f
kazuma_ohura <k.ohura0303@gmail.com>
2019-11-19 15:27:19 +0900

    テスト用の演算子作成。（空関数）

--- IncrementOperator/CoordinateX.h
+++ IncrementOperator/CoordinateX.h
@@ -6,6 +6,14 @@ public:
 	explicit CoordinateX(int in_value):value(in_value) {}
 	int GetValue() { return value; }
 
+	CoordinateX operator++() {
+		//return CoordinateX(this->value += 1);
+	}
+
+	CoordinateX operator++(int x) {
+		//return CoordinateX(this->value += 1);
+	}
+
 private:
 	int value;
 };
