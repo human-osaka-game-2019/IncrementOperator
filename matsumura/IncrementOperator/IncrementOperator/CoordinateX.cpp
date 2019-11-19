0772cb0d996cd7970cf087e2f2b601a1fe67f496
taishi-matsumura <taishi0706@gmail.com>
2019-11-19 17:53:49 +0900

    :sparkles: CoordinateXクラスを新規作成

---
+++ matsumura/IncrementOperator/IncrementOperator/CoordinateX.cpp
@@ -0,0 +1,14 @@
+﻿#include "CoordinateX.h"
+
+CoordinateX::CoordinateX(int x)
+	: value(x) {
+
+}
+
+CoordinateX& CoordinateX::operator++() {
+	return *this;
+}
+
+CoordinateX CoordinateX::operator++(int) {
+	return *this;
+}
---
+++ matsumura/IncrementOperator/IncrementOperator/CoordinateX.h
@@ -0,0 +1,25 @@
+﻿#ifndef COORDINATE_X_H_
+#define COORDINATE_X_H_
+
+class CoordinateX {
+public:
+	CoordinateX(int x);
+	int Value() { return value; }
+
+	/**
+	 * @brief	インクリメント演算子(前置)
+	 * @returns	自分自身
+	 */
+	CoordinateX& operator++();
+
+	/**
+	 * @brief	インクリメント演算子(後置)
+	 * @returns	インクリメント前のインスタンス
+	 */
+	CoordinateX operator++(int);
+
+private:
+	int value;
+};
+
+#endif
